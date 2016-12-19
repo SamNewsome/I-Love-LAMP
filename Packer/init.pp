e8c25e58e955625082a3381ecc32a975911aee51
[Bullhorn] <Timothy.Goodwin@qa.com>
2016-12-19 10:41:14 +0000

    updating packer init.pp file

--- Packer/init.pp
+++ Packer/init.pp
@@ -1,8 +1,27 @@
 class packer (
 
 $packer_archive = 'packer_0.12.1_linux_amd64.zip',
-$nexus_home = '/opt/pacler_0.12.1',
-$nexus_folder = 'packer_0.12.1',
-$install_location = '/opt/'
-)
+$packer_home = '/opt/packer_0.12.1',
+$packer_folder = 'packer_0.12.1',
+$install_location = '/opt/'){
+
+Exec {
+path => ['/usr/bin', '/usr/sbin', '/bin'],
+}
+
+
+file { "/opt/${packer_archive}":
+ensure => present,
+source => "puppet:///modules/packer/${packer_archive}",
+owner => vagrant,
+mode => 755,
+}
+
+exec { 'extract packer':
+cwd => '/opt/',
+command => "sudo tar zxvf ${packer_archive}",
+require => File["/opt/${packer_archive}"]
+}
+
+}
 
