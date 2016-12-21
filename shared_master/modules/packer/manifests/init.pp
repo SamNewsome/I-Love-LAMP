5efb81c195f5319b7b0b26dec5fc1ce115d1139c
[Bullhorn] <Timothy.Goodwin@qa.com>
2016-12-21 13:50:06 +0000

    updated packer init.pp

--- shared_master/modules/packer/manifests/init.pp
+++ shared_master/modules/packer/manifests/init.pp
@@ -17,7 +17,6 @@ command => "sudo rm -r /opt/packer",
 exec { "create packer directory in opt" :
 cwd => '/opt/',
 command => "sudo mkdir packer",
-require => Exec["remove packer directory in opt"],
 }
 
 exec { "copy zip file" :
