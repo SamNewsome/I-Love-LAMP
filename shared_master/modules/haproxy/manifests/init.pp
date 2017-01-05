7abaefe107783b32436918c5631458f714572616
cdee93 <chachris.darninsuang@qa.com>
2017-01-05 16:02:47 +0000

    Update init.pp

--- shared_master/modules/haproxy/manifests/init.pp
+++ shared_master/modules/haproxy/manifests/init.pp
@@ -1,72 +1,75 @@
 class haproxy {
-	
+
 Exec {
 path => ['/usr/bin', '/usr/sbin', '/bin'],
 }
 
 exec { "Install packages" :
-	command => "sudo apt-get -y install build-essential libssl-dev libpcre++-dev",
+        command => "sudo apt-get -y install build-essential libssl-dev libpcre++-dev",
 }
 
-exec { "remove user" :
-	command => "sudo userdel haproxy",
-	before => Exec['Add haproxy user'],
-}
+#exec { "remove user" :
+#       command => "sudo userdel haproxy",
+#       before => Exec['Add haproxy user'],
+#}
 
 exec { "Add haproxy user" :
-	command => "sudo useradd haproxy",
+        command => "sudo useradd haproxy",
 }
 
 exec { "Copy haproxy tar file" :
-        command => "sudo cp /tmp/shared/haproxy-1.7.1.tar.gz /opt",
-	require => Exec['Add haproxy user'],
+        command => "sudo cp /etc/puppet/modules/haproxy/files/haproxy-1.7.1.tar.gz /var/lib/haproxy-1.7.1.tar.gz
+        require => Exec['Add haproxy user'],
 }
 
 exec { "Extract haproxy tar file" :
-        cwd => "/opt",
-	command => "sudo tar -zxvf haproxy-1.7.1.tar.gz",
+        cwd => "/var/lib",
+        command => "sudo tar -zxvf haproxy-1.7.1.tar.gz",
         require => Exec['Copy haproxy tar file'],
 }
 
 exec { "Compile haproxy" :
-	cwd => "/opt/haproxy-1.7.1",
+        cwd => "/var/lib/haproxy-1.7.1",
         command => "sudo make TARGET=linux2628 CPU=native USE_STATIC_PCRE=1 USE_OPENSSL=1 USE_ZLIB=1",
         require => Exec['Extract haproxy tar file'],
 }
 
 exec { "Install haproxy" :
-        cwd => "/opt/haproxy-1.7.1",
-	command => "sudo make install",
+        cwd => "/var/lib/haproxy-1.7.1",
+        command => "sudo make install",
         require => Exec['Compile haproxy'],
 }
 
-file { '/etc/init.d/haproxy' :
-	ensure => present,
-	source => 'puppet:///modules/haproxy/haproxy',
-	require => Exec['Install haproxy'],
-	before => Exec['haproxy executable'],
-}
-
-exec { 'haproxy executable' :
-	cwd => '/etc/init.d',
-	command => 'sudo chmod a+x /etc/init.d/haproxy',
-	require => File['/etc/init.d/haproxy'],
+exec { "copy executable haproxy file" :
+        cwd => '/etc/init.d',
+        command => 'sudo cp /etc/puppet/modules/haproxy/files/haproxy haproxy',
+        require => Exec['Install haproxy'],
 }
 
 exec { "Create haproxy folder" :
-        command => "sudo mkdir /etc/haproxy",
-        require => Exec['haproxy executable'],
+        command => 'sudo mkdir /etc/haproxy',
+        require => Exec['copy executable haproxy file'],
 }
-	
-file { '/etc/haproxy/haproxy.cfg' :
-        ensure => present,
-        source => 'puppet:///modules/haproxy/haproxy.cfg',
+
+exec { "copy haproxy.cfg file" :
+        cwd => '/etc/haproxy',
+        command => 'sudo cp /etc/puppet/modules/haproxy/files/haproxy.cfg haproxy.cfg',
         require => Exec['Create haproxy folder'],
+}
+
+exec { "copy files part1" :
+        command => "sudo cp /var/lib/haproxy-1.7.1/haproxy /usr/sbin/haproxy",
+        require => Exec['copy haproxy.cfg file'],
+}
+
+exec { "copy files part2" :
+        command => "sudo cp /var/lib/haproxy-1.7.1/haproxy-systemd-wrapper /usr/sbin/haproxy-systemd-wrapper",
+        require => Exec['copy files part1'],
 }
 
 exec { "Start haproxy" :
         command => "sudo service haproxy start",
-        require => File['/etc/haproxy/haproxy.cfg'],
+        require => Exec['copy files part2'],
 }
 
 }
